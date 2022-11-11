b3f0ddb9eb7bfba4db72ff4dc958d288783cc409
Kotulski, Damian <damian.kotulski@dieboldnixdorf.com>
2022-11-11 12:44:02 +0100

    Styled components added, sample buttons created

---
+++ favnote/src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div className="Root">
+    <h1>Hello</h1>
+    <Button>close / save</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
+
+export default Root;
